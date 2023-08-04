09c2484d1ad25aead1f81c32777497be9c359caa
Luiz Pereira <leolima.ef@gmail.com>
2023-08-04 02:00:10 +0100

    Form Ready

--- src/app/components/NifValidator/nif-validator.ts
+++ src/app/components/NifValidator/nif-validator.ts
@@ -1,4 +1,3 @@
-import { Component, OnInit } from '@angular/core';
 import {  ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
 
 export function validateNIF(): ValidatorFn {
--- src/app/components/NumberValidator/numbervalidator.ts
+++ src/app/components/NumberValidator/numbervalidator.ts
@@ -1,4 +1,3 @@
-
 import { AbstractControl, ValidationErrors } from '@angular/forms';
 
 export function validatePhoneNumber(control: AbstractControl): ValidationErrors | null {
